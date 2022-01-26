cee9264ecc8fadc186ddc2943ff1cfc7fb7d6b54
Brandyn <stateofw@gmail.com>
2022-01-25 19:23:19 -0500

    mvp completed

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,11 +1,23 @@
 const router = require("express").Router();
 const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
-const { JWT_SECRET } = require("../secrets"); // use this secret!
+const { JWT_SECRET, BCRYPT_ROUNDS } = require("../secrets"); // use this secret!
+const Users = require("../users/users-model")
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
 
-router.post("/register", validateRoleName, (req, res, next) => {
+router.post("/register",validateRoleName , (req, res, next) => {
+  let newUser = req.body
+  
+  const hash = bcrypt.hashSync(newUser.password, BCRYPT_ROUNDS)
+  newUser.password = hash
+
+  Users.add(newUser)
+    .then(savedUser => {
+      res.status(201).json(savedUser)
+    })
+    .catch(next) 
   /**
     [POST] /api/auth/register { "username": "anna", "password": "1234", "role_name": "angel" }
-
     response:
     status 201
     {
@@ -18,19 +30,31 @@ router.post("/register", validateRoleName, (req, res, next) => {
 
 
 router.post("/login", checkUsernameExists, (req, res, next) => {
+  let { username, password } = req.body
+  
+  Users.findBy({'u.username': username})
+    .then(([user]) => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = makeToken(user)
+        res.status(200).json({ 
+          message: `${user.username} is back`,
+          token 
+        })
+      } else {
+        next({ status: 401, message: 'Invalid Credentials' })
+      }
+    })
+    .catch(next)
   /**
     [POST] /api/auth/login { "username": "sue", "password": "1234" }
-
     response:
     status 200
     {
       "message": "sue is back!",
       "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ETC.ETC"
     }
-
     The token must expire in one day, and must provide the following information
     in its payload:
-
     {
       "subject"  : 1       // the user_id of the authenticated user
       "username" : "bob"   // the username of the authenticated user
@@ -38,5 +62,17 @@ router.post("/login", checkUsernameExists, (req, res, next) => {
     }
    */
 });
+
+function makeToken(user){
+  const payload = {
+    subject: user.user_id,
+    username: user.username,
+    role_name: user.role_name
+  }
+  const options = {
+    expiresIn: "1d"
+  }
+  return jwt.sign(payload,JWT_SECRET,options)
+}
 
 module.exports = router;
